54aa424319fdd724fd93c02c82422eedeed46acc
kuno <neokuno@gmail.com>
2013-01-08 15:25:18 +0800

    nts

--- demo/routes/index.js
+++ demo/routes/index.js
@@ -19,9 +19,6 @@ exports.create = function(req, res, next) {
 
   e.addVhost(host, config).then(function() {
     res.redirect('/admin/' + host);
-  },
-  function() {
-    e.restart();
   }).fail(function(reason) {
     next(new Error.HTTP(reason, 500));
   });
