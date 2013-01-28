0e554973cd73e33200771edee01d1594780b6115
Guan Qing <neokuno@gmail.com>
2013-01-28 17:37:39 +0800

    Make debug not default

--- demo/routes/index.js
+++ demo/routes/index.js
@@ -4,7 +4,7 @@
  */
 
 var Ejabberd = require('../..');
-var e = new Ejabberd();
+var e = new Ejabberd(null, {debug:true});
 
 exports.index = function(req, res){
   res.render('index', { title: 'Github Chat' });
