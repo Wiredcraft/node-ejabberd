37304dbb1236a3f43378295c1257804812668a0f
Guan Qing <neokuno@gmail.com>
2013-01-28 15:21:00 +0800

    add callback test

---
+++ test/callback.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var cp = require('child_process');
+var success = 0;
+var start = Date.now();
+
+for (var i = 0; i < 10; ++i) {
+  cp.exec('/etc/init.d/ejabberd restart', function(err, stderr, stdout) {
+    if (err) throw err;
+
+    ++success;
+  });
+}
+
+
+for (Date.now() - start < 1000 * 10) {}
+
+
+console.log('Success time is $s', success);
