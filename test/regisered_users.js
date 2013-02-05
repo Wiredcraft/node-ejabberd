a3640ed3fb4684447f6aa80f4831f404dbb1a0d1
Guan Qing <neokuno@gmail.com>
2013-02-05 10:49:26 +0800

    fix

--- test/regisered_users.js
+++ test/regisered_users.js
@@ -4,11 +4,11 @@ var host = process.argv[2];
 var child = cp.spawn('ejabberdctl', ['registered_users', host]);
 
 child.stderr.on('data', function(data) {
-  console.log('stderr: ', data);
+  console.log('stderr: ', data.toString());
 });
 
 child.stdout.on('data', function(data) {
-  console.log('stdout: ', data);
+  console.log('stdout: ', data.toString());
 });
 
 child.on('exit', function(code) {
